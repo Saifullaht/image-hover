dcd572c1b2ca9e0939fcce7c1c1dc0b608ee864d
Saifullaht <mrsaifullah025@gmail.in>
2024-06-13 10:57:22 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,42 @@
+//// card 1 //////////////////////
+function newimage1() {
+    document.getElementById("imag1").src = " https://tse1.mm.bing.net/th?id=OIP.L2agUq0OMZK59H3NqLUYNwHaE8&pid=Api&P=0&h=220"
+}
+function  oldimage1() {
+document.getElementById("imag1").src = " https://img-ik.cars.co.za/news-site-za/images/2020/08/thar008.jpg?tr=w-1200,h-800"
+}
+
+// ////// card 2 //////////////////////
+
+function newimage2() {
+    document.getElementById("imag2").src = "  https://www.siambodykit.com/wp-content/uploads/2021/06/fortuner-legendet-lgd-w-siam-bodykit-5-1536x1024.jpg"
+}
+function  oldimage2() {
+document.getElementById("imag2").src = " https://tse2.mm.bing.net/th?id=OIP.9hpL-eBZRi_t3tgP6CdBXAHaE8&pid=Api&P=0&h=220"
+}
+
+// ////// card 3 //////////////////////
+
+function newimage3() {
+    document.getElementById("imag3").src = " https://tse3.mm.bing.net/th?id=OIP.g4rU4nD0cGBYuTRr8yoncgAAAA&pid=Api&P=0&h=220"
+}
+function  oldimage3() {
+document.getElementById("imag3").src = " http://st.automobilemag.com/uploads/sites/11/2017/06/2018-Dodge-Challenger-SRT-Hellcat-rear-three-quarter-01.jpg"
+}
+
+// ////// card 4 //////////////////////
+function newimage4() {
+    document.getElementById("imag40").src = " https://tse3.mm.bing.net/th?id=OIP.g4rU4nD0cGBYuTRr8yoncgAAAA&pid=Api&P=0&h=220"
+}
+function  oldimage4() {
+document.getElementById("imag40").src = "  https://uncrate.com/p/2021/02/audi-e-tron-gt-4-4.jpg"
+}
+
+// ////// card 5 //////////////////////
+
+function newimage5() {
+    document.getElementById("imag5").src = " https://i.ytimg.com/vi/D5sPjkSjCgE/maxresdefault.jpg"
+}
+function  oldimage5() {
+document.getElementById("imag5").src = " http://cdn.shopify.com/s/files/1/0278/9159/7379/articles/WhatsApp_Image_2022-09-21_at_5.30.29_PM_1024x1024.jpg?v=1663822929"
+}
